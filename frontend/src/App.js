ae4ac653b6cde3ad84c0f27950851474c481bb69
Vishwajeet Raj <vishwajeetraj11@gmail.com>
2020-12-20 23:54:24 +0530

    Added Bootstrap, Added Products Data

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,17 @@
-
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+import {Container} from "react-bootstrap"
 
 function App() {
   return (
     <>
-      <h1>Welcome To Proshop</h1>
+    <Header />
+    <main className="py-3">
+    <Container>
+    <h1>Welcome To Proshop</h1>
+    </Container>
+    </main>
+    <Footer />
     </>
   );
 }
