04abe44da8257aa9e722509c344bdc32985a1c2f
Vishwajeet Raj <vishwajeetraj11@gmail.com>
2021-02-03 20:32:10 +0530

    BACKEND: Added getUserById and updateUserById routes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -120,6 +120,46 @@ const deleteUser = asyncHandler(async (req, res) => {
   }
 })
 
+// @description    GET User by ID
+// @route          GET /api/users/:id
+// @access         Private/Admin
+const getUserById = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id).select('-password')
+  if (user) {
+    res.json(user)
+  } else {
+    res.status(404)
+    throw new Error("User not found")
+  }
+})
+
+// @description    UPDATE user by ID
+// @route          PUT /api/users/:id
+// @access         Private/Admin
+const updateUserById = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.params.id)
+  if (user) {
+    user.name = req.body.name || user.name
+    user.email = req.body.email || user.email
+    
+    if(req.body.isAdmin !== undefined && req.body.isAdmin !== null) {
+      user.isAdmin = req.body.isAdmin
+    }
+
+    const updatedUser = await user.save()
+
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+    })
+  } else {
+    res.status(404)
+    throw new Error("User not found")
+  }
+})
+
 export {
   authUser,
   getUserProfile,
@@ -127,4 +167,6 @@ export {
   updateUserProfile,
   getUser,
   deleteUser,
+  getUserById,
+  updateUserById
 }
