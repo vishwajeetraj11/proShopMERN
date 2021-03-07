a42c767977efcf9ab5b327d66cff25604f02562b
Vishwajeet Raj <vishwajeetraj11@gmail.com>
2021-03-07 20:33:00 +0530

    frontend: create Product functionality

--- frontend/src/screens/ProductlistScreen.js
+++ frontend/src/screens/ProductlistScreen.js
@@ -3,8 +3,9 @@ import { Table, Button, Row, Col } from "react-bootstrap"
 import { useDispatch, useSelector } from "react-redux"
 import Message from "../components/Message"
 import Loader from "../components/Loader"
-import { listProducts, deleteProduct } from "../actions/productActions"
+import { listProducts, deleteProduct, createProduct } from "../actions/productActions"
 import { LinkContainer } from "react-router-bootstrap"
+import {PRODUCT_CREATE_RESET} from "../constants/productConstants"
 
 const ProductListScreen = ({ history, match }) => {
   const dispatch = useDispatch()
@@ -14,6 +15,9 @@ const ProductListScreen = ({ history, match }) => {
 
   const productDelete = useSelector((state) => state.productDelete)
   const { loading: loadingDelete, error: errorDelete, success: successDelete } = productDelete
+
+  const productCreate = useSelector((state) => state.productCreate)
+  const { loading: loadingCreate, error: errorCreate, success: successCreate, product: createdProduct } = productCreate
 
   const userLogin = useSelector((state) => state.userLogin)
   const { userInfo } = userLogin
@@ -25,16 +29,22 @@ const ProductListScreen = ({ history, match }) => {
     }
   }
   const createProductHandler = () => {
-    // dispatch(createProduct())
+    dispatch(createProduct())
   }
 
   useEffect(() => {
-    if (userInfo && userInfo.isAdmin) {
-      dispatch(listProducts())
-    } else {
-      history.push("/login")
+    dispatch({type: PRODUCT_CREATE_RESET})
+
+    if (!userInfo.isAdmin) {
+      history.push('/login')
     }
-  }, [dispatch, history, userInfo, successDelete])
+
+    if(successCreate) {
+      history.push(`/admin/product/${createdProduct._id}`)
+    } else {
+      dispatch(listProducts())
+    }
+  }, [dispatch, history, userInfo, successDelete, successCreate, createdProduct])
 
   return (
     <>
@@ -50,6 +60,8 @@ const ProductListScreen = ({ history, match }) => {
       </Row>
       {loadingDelete && <Loader />}
       {errorDelete && <Message variant="danger">{errorDelete}</Message>}
+      {loadingCreate && <Loader />}
+      {errorCreate && <Message variant="danger">{errorCreate}</Message>}
       {loading ? (
         <Loader />
       ) : error ? (
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -6,7 +6,8 @@ import { composeWithDevTools } from "redux-devtools-extension"
 import {
   productListReducer,
   productDetailsReducer,
-  productDeleteReducer
+  productDeleteReducer,
+  productCreateReducer
 } from "./reducers/productReducers"
 import { cartReducer } from "./reducers/cartReducers"
 import {
@@ -30,6 +31,7 @@ const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
   productDelete: productDeleteReducer, 
+  productCreate: productCreateReducer, 
   cart: cartReducer,
   userLogin: userLoginReducer,
   userSignup: userSignupReducer,
